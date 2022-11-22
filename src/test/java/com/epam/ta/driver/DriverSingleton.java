c99f2c65bc5b900071337570c8fd75d639dfb493
Dikonbayev Otabek <111531481+DikonbayevOtabek@users.noreply.github.com>
2022-11-22 11:40:28 +0500

    Add files via upload

---
+++ src/test/java/com/epam/ta/driver/DriverSingleton.java
@@ -0,0 +1,56 @@
+package com.epam.ta.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+
+/*
+switch (System.getProperty("browser")){
+                case "firefox": {
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver(firefoxOptions);
+                }
+                default: {
+                    ChromeOptions chromeOptions= new ChromeOptions();
+                    chromeOptions.setBinary("C:\\Users\\Otabek\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(chromeOptions);
+                }
+            }
+
+ */
+public class DriverSingleton {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            switch (System.getProperty("browser")){
+                case "firefox": {
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver(firefoxOptions);
+                }
+                default: {
+                    ChromeOptions chromeOptions= new ChromeOptions();
+                    chromeOptions.setBinary("C:\\Users\\Otabek\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(chromeOptions);
+                }
+            }
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        driver.quit();
+        driver = null;
+    }
+}
---
+++ src/test/java/com/epam/ta/page/GmailMainPage.java
@@ -0,0 +1,50 @@
+package com.epam.ta.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GmailMainPage extends AbstractPage {
+    private final String BASE_URL = "https://gmail.com";
+    private WebDriverWait wait = new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS);
+
+    @FindBy(xpath = "//div[@style='user-select: none']")
+    WebElement createNewMessageButton;
+    @FindBy(xpath = "//span[@class='nU ']")
+    WebElement newMessageButtonGmail;
+
+
+    public GmailMainPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(this.driver, this);
+    }
+
+    public CreateNewEmailPage invokeNewEmailCreation(){
+        wait.until(ExpectedConditions.elementToBeClickable(createNewMessageButton));
+        createNewMessageButton.click();
+        return new CreateNewEmailPage(driver);
+    }
+    public boolean isButtonVisible(){
+        try {
+            wait.until(ExpectedConditions.visibilityOf(newMessageButtonGmail));
+            return newMessageButtonGmail.isDisplayed();
+        }catch (Exception exception) {
+            return false;
+        }
+    }
+
+    @Override
+    public GmailMainPage openPageGmail() {
+        driver.navigate().to(BASE_URL);
+        return this;
+    }
+
+    @Override
+    public GmailMainPage openPageMail() {
+        return null;
+    }
+}
